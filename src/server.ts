61be389e32db528b3961efed4e9c9500974f1bc2
Timothy Lee <timothy.lee@pepperstone.com>
2019-01-23 14:49:33 +1100

    Changes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
-import express from 'express';
-const app = express();
+import app from './app'
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const PORT = process.env.PORT || '5000';
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
