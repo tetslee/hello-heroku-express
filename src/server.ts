bf2a26f3c2f3f2a9ea9aa02123689491af2ac4cb
Timothy Lee <timothy.lee@pepperstone.com>
2019-01-23 17:34:10 +1100

    stuff

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,25 @@
 import app from './app'
+import http from 'http';
+import { Client } from 'pg';
 
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || '5000';
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+const server = http.createServer((req, res) => {
+  const client = new Client({
+    connectionString: DATABASE_URL,
+  });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  client.connect()
+    .then(() => client.query('SELECT * FROM hellotable'))
+    .then((result) => {
+      res.end(`${result.rows[0].name}\n`);
+      client.end();
+    })
+    .catch(() => {
+      res.end('ERROR');
+      client.end();
+    });
+});
